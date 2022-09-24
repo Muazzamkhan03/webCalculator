2654cf9f43d9e2ac7ad5f0bfdf20404cee95c37d
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-09-24 17:15:26 +0500

    Made the del button functional

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ btns.forEach(btn=>{
             str = "";
             document.querySelector("#ans").innerHTML = str;
         }
+        else if(btn.value == "del"){
+            str = document.querySelector("#ans").innerHTML;
+            let newStr = "";
+            if(str.length == 1){
+                str = "";
+                document.querySelector("#ans").innerHTML = str;
+            }
+            else{
+                for(let i=0; i<(str.length)-1; i++){
+                    newStr += str[i];
+                    document.querySelector("#ans").innerHTML = newStr;
+                }
+            }
+
+        }
         else{
             str+= btn.value;
             document.querySelector("#ans").innerHTML = str;
