af6e2adf9dde26ffc6effd294a4bb70a6da1c8ff
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-09-24 15:32:16 +0500

    Adding a function so that the button does something when hovered on

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 
-btns = document.querySelectorAll(".btn");
-
-btns.forEach(btn => {
-    btn.addEventListener("mouseover", ()=>{
-        btn.style.cursor = "pointer";
-        btn.style.transform = "scale(2,2)"
-    });
-});
